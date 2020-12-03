dfa5d8835bd9210466581838775f50295e3337b1
Drew Jackson <anjackson@riotgames.com>
2020-12-02 21:29:08 -0800

    Part 3 - Where I learned I've forgotten how to do slopes

--- 2020/3/2020.3.py
+++ 2020/3/2020.3.py
@@ -2,22 +2,27 @@ import os
 import re
 
 
-def part1(values):
-    validCount = 0
-    for value in values:
-        match = re.match("(\d*)-(\d*) ([a-zA-Z]): ([a-zA-Z]*)", value)
-        lowBound = int(match.group(1))
-        highBound = int(match.group(2))
-        policyLetter = match.group(3)
-        password = match.group(4)
+def part1(mapArray, xslope, yslope):
+    # Go from 0,0 to 3, 1 until end of lines
+    xpos = 0
+    ypos = 0
+    treeCount = 0
+    for row in mapArray:
+        if (ypos % yslope == 0):
+            tobagganValue = row[xpos]
+            if tobagganValue == '#': #tree
+                treeCount += 1
+                row = row[:xpos] + 'X' + row[xpos+1:]
+            else:
+                row = row[:xpos] + 'O' + row[xpos+1:]
+            xpos += xslope
+        ypos += 1
+        if xpos >= len(row) - 1:
+            xpos -= len(row) - 1
+        print(row, end='')
+    print(f"Tree count: {treeCount}", "")
+    return treeCount
 
-        #passwordValid = re.match(f"{policyLetter}{{{lowBound},{highBound}}}", password)
-        policyLetterCount = password.count(policyLetter)
-        passwordValid = lowBound <= policyLetterCount and policyLetterCount <= highBound
-        if passwordValid:
-            validCount += 1
-            print(f"Valid: {lowBound} to {highBound} has {password} with {policyLetter}")
-    print(f"Valid count: {validCount}")
 
 
 def part2(values):
@@ -30,7 +35,7 @@ def part2(values):
         password = match.group(4)
 
         #passwordValid = re.match(f"{policyLetter}{{{lowBound},{highBound}}}", password)
-         
+        
         passwordValid = bool(password[index1 - 1] == policyLetter) ^ bool(password[index2 - 1] == policyLetter)
         if passwordValid:
             validCount += 1
@@ -38,20 +43,27 @@ def part2(values):
     print(f"Valid count: {validCount}")
 
 
+def parse_map(lines):
+    mapArray = []
+    for line in lines:
+        mapArray.insert(line)
+    return mapArray
+
 def read_file():
     file = os.path.join(os.path.dirname(__file__), 'input.txt')
     lines = ''
     values = set()
     with open(file, 'r') as file:
         lines = file.readlines()
-    
-        
-    
-    #part1(lines)
-    part2(lines)
-    
-    
+    #mapArray = parse_map(lines)
+    #part1(lines)        
+    slope1 = part1(lines, 1, 1)
+    slope2 = part1(lines, 3, 1)
+    slope3 = part1(lines, 5, 1)
+    slope4 = part1(lines, 7, 1)
+    slope5 = part1(lines, 1, 2)
 
+    print(f"{slope1} {slope2} {slope3} {slope4} {slope5} = {slope1 * slope2 * slope3 * slope4 * slope5}")
 
 # clean this up later
 read_file()
