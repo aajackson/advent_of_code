3d96b0837a5a07f7f0525e7b6eefabde29cc029d
Drew Jackson <anjackson@riotgames.com>
2020-12-02 21:54:28 -0800

    Minor cleanup

--- 2020/3/2020.3.py
+++ 2020/3/2020.3.py
@@ -3,7 +3,6 @@ import re
 
 
 def part1(mapArray, xslope, yslope):
-    # Go from 0,0 to 3, 1 until end of lines
     xpos = 0
     ypos = 0
     treeCount = 0
@@ -20,28 +19,8 @@ def part1(mapArray, xslope, yslope):
         if xpos >= len(row) - 1:
             xpos -= len(row) - 1
         print(row, end='')
-    print(f"Tree count: {treeCount}", "")
+    print(f"\nTree count: {treeCount}")
     return treeCount
-
-
-
-def part2(values):
-    validCount = 0
-    for value in values:
-        match = re.match("(\d*)-(\d*) ([a-zA-Z]): ([a-zA-Z]*)", value)
-        index1 = int(match.group(1))
-        index2 = int(match.group(2))
-        policyLetter = match.group(3)
-        password = match.group(4)
-
-        #passwordValid = re.match(f"{policyLetter}{{{lowBound},{highBound}}}", password)
-        
-        passwordValid = bool(password[index1 - 1] == policyLetter) ^ bool(password[index2 - 1] == policyLetter)
-        if passwordValid:
-            validCount += 1
-            print(f"Password valid with letter {policyLetter} found at [{index1}] xor [{index2}] in {password}")
-    print(f"Valid count: {validCount}")
-
 
 def parse_map(lines):
     mapArray = []
@@ -55,15 +34,13 @@ def read_file():
     values = set()
     with open(file, 'r') as file:
         lines = file.readlines()
-    #mapArray = parse_map(lines)
-    #part1(lines)        
     slope1 = part1(lines, 1, 1)
-    slope2 = part1(lines, 3, 1)
+    slope2 = part1(lines, 3, 1) # part 1 answer
     slope3 = part1(lines, 5, 1)
     slope4 = part1(lines, 7, 1)
     slope5 = part1(lines, 1, 2)
 
-    print(f"{slope1} {slope2} {slope3} {slope4} {slope5} = {slope1 * slope2 * slope3 * slope4 * slope5}")
+    print(f"{slope1} * {slope2} * {slope3} * {slope4} * {slope5} = {slope1 * slope2 * slope3 * slope4 * slope5}")
 
 # clean this up later
 read_file()
