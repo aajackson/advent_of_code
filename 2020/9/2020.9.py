f01a99fdf491b3d5761cc960f46b97582ee9ddb2
Drew Jackson <anjackson@riotgames.com>
2020-12-09 17:38:58 -0800

    Day 9. Nothing smart, just let the computer do the hard work instead.

--- 2020/9/2020.9.py
+++ 2020/9/2020.9.py
@@ -2,22 +2,57 @@ import os
 import re
 import requests
 import utils
+import sys
 
+
+def check_prev_x(arr, index, searchRange = 25):
+    targetValue = arr[index]
+    searchIndex = index - searchRange
+    for searchValue in arr[searchIndex:index]:
+        for otherValue in arr[searchIndex+1:index]:
+            if searchValue + otherValue == targetValue:
+                return True
+    # no value found
+    return False
+
+def sum_window(arr, windowStart, windowEnd):
+    sumValue = 0
+    minValue = sys.maxsize
+    maxValue = 0
+    for value in arr[windowStart:windowEnd]:
+        sumValue += value
+        if minValue > value:
+            minValue = value
+        if maxValue < value:
+            maxValue = value
+    return sumValue, minValue, maxValue
 
 def read_file():
     rawInput = utils.get_input("2020", "9")
-    data = rawInput.split("\n")
+    data = list(map(lambda x: int(x), rawInput.split("\n")))
 
     # process data
-
     # part 1
-    part1Answer = 0
-    print(f"Part 1: {part1Answer}")
-
+    for index in range(25, len(data)):
+        if not check_prev_x(data, index):
+            print(f"Value not found! {data[index]}")
 
     # part 2
-    part2Answer = 0
-    print(f"Part 2: {part2Answer}")
+    # create a sliding window to find the range
+    # where the sum is the targetValue
+    targetValue = 27911108 #from part 1
+    windowStart = 0
+    windowEnd = 1
+    windowSum, minValue, maxValue = sum_window(data, windowStart, windowEnd)
+    while windowSum != targetValue:
+        if windowSum < targetValue:
+            windowEnd += 1
+        if windowSum > targetValue:
+            windowStart += 1
+        windowSum, minValue, maxValue = sum_window(data, windowStart, windowEnd)
+    print(f"Window range found! [{windowStart} - {windowEnd}]. Min + max: {minValue} + {maxValue} = {minValue + maxValue}")
+
+
     
 
 # clean this up later
